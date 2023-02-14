86196dcf352909dfda1e53220ccd83948db1f6f5
amanibhavam <iam@defn.sh>
2023-02-14 00:06:57 +0000

    export kustomize wrapper

--- flake.nix
+++ flake.nix
@@ -1,23 +1,31 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
+    dev.url = github:defn/pkg/dev-0.0.23?dir=dev;
     kubernetes.url = github:defn/pkg/kubernetes-0.0.6?dir=kubernetes;
   };
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
-
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
-
-    config = rec {
-      slug = builtins.readFile ./SLUG;
-      version = builtins.readFile ./VERSION;
-    };
-
-    handler = { pkgs, wrap, system, builders }: rec {
-      defaultPackage = wrap.nullBuilder { 
+  outputs = inputs:
+    let
+      kustomize = { src, wrap }: wrap.bashBuilder {
         propagatedBuildInputs = wrap.flakeInputs;
+
+        inherit src;
+
+        installPhase = ''
+          mkdir -p $out
+          kustomize build --enable-helm local > $out/main.yaml
+        '';
+      };
+    in
+    { inherit kustomize; } // inputs.dev.main rec {
+      inherit inputs;
+
+      src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+
+      handler = { pkgs, wrap, system, builders, commands, config }: rec {
+        defaultPackage = wrap.nullBuilder {
+          propagatedBuildInputs = wrap.flakeInputs;
+        };
       };
     };
-  };
 }
