d3f35733301c8d256caabbb0455bdcc813833289
amanibhavam <iam@defn.sh>
2023-02-14 00:13:43 +0000

    argo workflows flake

---
+++ k/argo-workflows/flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    dev.url = github:defn/pkg/dev-0.0.23?dir=dev;
+    app.url = github:defn/app/0.0.3;
+  };
+
+  outputs = inputs: inputs.dev.main rec {
+    inherit inputs;
+
+    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+
+    handler = { pkgs, wrap, system, builders, commands, config }: rec {
+      defaultPackage = inputs.app.kustomize { inherit src; inherit wrap; };
+    };
+  };
+}
