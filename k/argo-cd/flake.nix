88be58ff3bd46a9c002b3a334ce5db8db3daa844
amanibhavam <iam@defn.sh>
2023-02-14 19:17:25 +0000

    argocd sugar

--- k/argo-cd/flake.nix
+++ k/argo-cd/flake.nix
@@ -1,16 +1,4 @@
 {
-  inputs = {
-    dev.url = github:defn/pkg/dev-0.0.23?dir=dev;
-    app.url = github:defn/app/0.0.4;
-  };
-
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
-
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
-
-    handler = { pkgs, wrap, system, builders, commands, config }: rec {
-      defaultPackage = inputs.app.kustomize { inherit src; inherit wrap; };
-    };
-  };
+  inputs.app.url = github:defn/app/0.0.5;
+  outputs = inputs: inputs.app.main rec { src = ./.; };
 }
