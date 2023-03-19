d4c720e278deb79dddde78e396db071ddee167ee
amanibhavam <iam@defn.sh>
2023-03-19 00:29:43 +0000

    bump

--- k/meh/flake.nix
+++ k/meh/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.170;
-    argo-cd.url = github:defn/app/argo-workflows-0.0.5?dir=k/argo-cd;
-    argo-workflows.url = github:defn/app/argo-workflows-0.0.5?dir=k/argo-workflows;
+    argo-cd.url = github:defn/app/argo-cd-0.0.7?dir=k/argo-cd;
+    argo-workflows.url = github:defn/app/argo-workflows-0.0.6?dir=k/argo-workflows;
   };
 
   outputs = inputs: inputs.pkg.main rec {
