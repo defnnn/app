a0b103ea1abd84970691276faec6fab49c6d4631
amanibhavam <iam@defn.sh>
2023-03-21 05:47:55 +0000

    upgrade k3d

--- k3d/flake.nix
+++ k3d/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.170;
     vault.url = github:defn/pkg/vault-1.13.0-2?dir=vault;
-    kubernetes.url = github:defn/pkg/kubernetes-0.0.16?dir=kubernetes;
+    kubernetes.url = github:defn/pkg/kubernetes-0.0.18?dir=kubernetes;
   };
 
   outputs = inputs: inputs.pkg.main rec {
