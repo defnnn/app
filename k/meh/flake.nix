4bb40d85b18cd8599babd858142704b4c0d40112
amanibhavam <iam@defn.sh>
2023-03-31 20:20:00 -0700

    udate pkg

--- k/meh/flake.nix
+++ k/meh/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    pkg.url = github:defn/pkg/0.0.170;
+    pkg.url = github:defn/pkg/0.0.172;
     argo-cd.url = github:defn/app/argo-cd-0.0.8?dir=k/argo-cd;
     argo-workflows.url = github:defn/app/argo-workflows-0.0.7?dir=k/argo-workflows;
   };
