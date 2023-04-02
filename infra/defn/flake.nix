08c2100519d17dccfcfc51f309a393336de05e71
amanibhavam <iam@defn.sh>
2023-04-02 06:21:36 +0000

    remove defn/lib refs

--- infra/defn/flake.nix
+++ infra/defn/flake.nix
@@ -1,7 +1,7 @@
 {
-  inputs.lib.url = github:defn/lib/0.0.75;
+  inputs.app.url = github:defn/app/0.0.20;
   inputs.infra.url = github:defn/app/infra-0.0.15?dir=cmd/infra;
-  outputs = inputs: inputs.lib.cdktfMain rec {
+  outputs = inputs: inputs.app.cdktfMain rec {
     src = ./.;
     infra = inputs.infra;
     infra_cli = "infra";
