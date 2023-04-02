a163986f354cfe3ea620083822b5d6f809906124
amanibhavam <iam@defn.sh>
2023-04-02 06:11:41 +0000

    fix inputs

--- infra/defn/flake.nix
+++ infra/defn/flake.nix
@@ -1,6 +1,6 @@
 {
-  inputs.lib.url = github:defn/lib/0.0.74;
-  inputs.infra.url = github:defn/app/infra-0.0.14?dir=cmd/infra;
+  inputs.lib.url = github:defn/lib/0.0.75;
+  inputs.infra.url = github:defn/app/infra-0.0.15?dir=cmd/infra;
   outputs = inputs: inputs.lib.cdktfMain rec {
     src = ./.;
     infra = inputs.infra;
