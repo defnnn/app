6fe9021c9abbaf8793a0707f3648f4abe66b92fa
amanibhavam <iam@defn.sh>
2023-04-04 00:15:45 -0700

    upgrade deps

--- infra/defn/flake.nix
+++ infra/defn/flake.nix
@@ -1,6 +1,6 @@
 {
-  inputs.app.url = github:defn/app/0.0.23;
-  inputs.infra.url = github:defn/app/infra-0.0.17?dir=cmd/infra;
+  inputs.app.url = github:defn/app/0.0.28;
+  inputs.infra.url = github:defn/app/infra-0.0.18?dir=cmd/infra;
   outputs = inputs: inputs.app.cdktfMain rec {
     src = ./.;
     infra = inputs.infra;
